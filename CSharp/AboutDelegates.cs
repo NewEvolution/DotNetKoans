36bbb16f0bcdac94aedf3c7d188561937ffa8ca8
Ryan Tanay <ryantanay@newevolution.org>
2015-10-20 15:29:45 -0500

    AboutDelegates completed

--- CSharp/AboutDelegates.cs
+++ CSharp/AboutDelegates.cs
@@ -130,7 +130,8 @@ namespace DotNetKoans.CSharp
 			Curry adding = f.Add5;
 			adding += f.Add10;
 			Assert.Equal(2, adding.GetInvocationList().Length);
-			//Remove Add5 from the invocation list
+            //Remove Add5 from the invocation list
+            adding -= f.Add5;
 			Assert.Equal(1, adding.GetInvocationList().Length);
 			Assert.Equal("Add10", adding.Method.Name);
 		}
@@ -157,21 +158,21 @@ namespace DotNetKoans.CSharp
 			//  public delgate void Action<T>(T obj);
 
 			Action<int> i = AssertIntEqualsFourtyTwo;
-			i((int)FILL_ME_IN);
+			i(42);
 		}
 		[Koan(13)]
 		public void BuiltInActionDelegateTakesString()
 		{
 			// Because the delegate is a template, it also works with any other type. 
 			Action<string> s = AssertStringEqualsFourtyTwo;
-			s((string)FILL_ME_IN);
+			s("42");
 		}
 		[Koan(14)]
 		public void BuiltInActionDelegateIsOverloaded()
 		{
 			//Action is an overloaded delegate so it can take more than one paramter
 			Action<int, string> a = AssertAddEqualsFourtyTwo;
-			a(12, (string)FILL_ME_IN);
+			a(12, "30");
 		}
 		public class Seen
 		{
@@ -194,7 +195,7 @@ namespace DotNetKoans.CSharp
 
 			Array.ForEach(greeting.ToCharArray(), s.Look);
 
-			Assert.Equal(FILL_ME_IN, s.Letters);
+			Assert.Equal("Hello world", s.Letters);
 		}
 
 		private bool IntEqualsFourtyTwo(int x)
@@ -213,14 +214,14 @@ namespace DotNetKoans.CSharp
 			//Predicate allows you to codify a condition and pass it around. 
 			//You use it to determine if an object satisfies some criteria. 
 
-			Predicate<int> i = (Predicate<int>)FILL_ME_IN;
+			Predicate<int> i = IntEqualsFourtyTwo;
 			Assert.True(i(42));
 		}
 		[Koan(17)]
 		public void BuiltInPredicateDelegateStringSatisfied()
 		{
 			//Because it is a template, you can work with any type
-			Predicate<string> s = (Predicate<string>)FILL_ME_IN;
+			Predicate<string> s = StringEqualsFourtyTwo;
 			Assert.True(s("42"));
 
 			//Predicate is not overloaded, so unlike Action<> you cannot do this...
@@ -238,7 +239,7 @@ namespace DotNetKoans.CSharp
 			//Predicate can be used to find an element in an array
 			var countries = new []{ "Greece", "Spain", "Uruguay", "Japan" };
 
-			Assert.Equal(FILL_ME_IN, Array.Find(countries, StartsWithS));
+			Assert.Equal("Spain", Array.Find(countries, StartsWithS));
 		}
 
 		private bool IsInSouthAmerica(string country)
@@ -252,7 +253,7 @@ namespace DotNetKoans.CSharp
 			//Predicate can also be used when verifying 
 			var countries = new[] { "Greece", "Spain", "Uruguay", "Japan" };
 
-			Assert.Equal(FILL_ME_IN, Array.TrueForAll(countries, IsInSouthAmerica));
+			Assert.Equal(false, Array.TrueForAll(countries, IsInSouthAmerica));
 		}
 
 		private string FirstMonth()
@@ -266,15 +267,16 @@ namespace DotNetKoans.CSharp
 		[Koan(20)]
 		public void FuncWithNoParameters()
 		{
-			//The Func<> delegate 
-			//  public delegate TResult Func<T, TResult>(T arg);
-			//Is very similar to the Action<> delegate. However, Func<> does not require any parameters, while does require returns a value.
+            //The Func<> delegate 
+            //  public delegate TResult Func<T, TResult>(T arg);
+            //Is very similar to the Action<> delegate. However, Func<> does not require any parameters,
+            //while does require returns a value.
 			//The last type parameter specifies the return type. If you only specify a single 
 			//type, Func<int>, then the method takes no paramters and returns an int.
 			//If you specify more than one parameter, then you are specifying the paramter types as well.
 
 			Func<string> d = FirstMonth;
-			Assert.Equal(FILL_ME_IN, d());
+			Assert.Equal("January", d());
 		}
 		[Koan(21)]
 		public void FunctionReturnsInt()
@@ -283,7 +285,7 @@ namespace DotNetKoans.CSharp
 			//The first type parameters define the parameter types and the last one is the return type. So the following matches
 			//a method which takes two int parameters and returns a int.
 			Func<int, int, int> a = Add;
-			Assert.Equal(FILL_ME_IN, a(1, 1));
+			Assert.Equal(2, a(1, 1));
 		}
 
 		public class Car
@@ -305,14 +307,16 @@ namespace DotNetKoans.CSharp
 		[Koan(22)]
 		public void SortingWithComparison()
 		{
-			//You could make classes sortable by implementing IComparable or IComparer. But the Comparison<> delegate makes it easier
-			//	public delegate int Comparison<T>(T x, T y);
-			//All you need is a method which takes two of the same type and returns -1, 0, or 1 depending upon what order they should go in.
+            //You could make classes sortable by implementing IComparable or IComparer.
+            //But the Comparison<> delegate makes it easier
+            //	public delegate int Comparison<T>(T x, T y);
+            //All you need is a method which takes two of the same type and returns -1, 0, or 1
+            //depending upon what order they should go in.
 			var cars = new[] { new Car("Alfa Romero", "GTV-6", 1986), new Car("BMC", "Mini", 1959) };
 			Comparison<Car> by = SortByModel;
 			Array.Sort(cars, by);
 
-			Assert.Equal(FILL_ME_IN, cars[0].Model);
+			Assert.Equal("GTV-6", cars[0].Model);
 		}
 
 		private string Stringify(int x)
@@ -330,7 +334,7 @@ namespace DotNetKoans.CSharp
 
 			var result = Array.ConvertAll(numbers, c);
 
-			Assert.Equal(FILL_ME_IN, result);
+			Assert.Equal(new[] { "1", "2", "3", "4" }, result);
 		}
 	}
 }
